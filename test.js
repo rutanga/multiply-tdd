84ae07debfb95b6534a3b93cf47a39bc07189849
rutanga <phiphirutanga@gmail.com>
2025-07-06 07:08:51 +0300

    Added another test for a function that doesn't return 1

--- test.js
+++ test.js
@@ -5,6 +5,11 @@ describe('multiply function', function () {
     it('should multiply 1 * 1 = 1', function () {
         assert.equal(multiply(1, 1), 1);
 
-    })
+    });
+
+    //adding another test for a function that doesn't return 1
+    it('should multiply 2 * 2 = 4', function () {
+        assert.equal(multiply(2, 2), 4);
+    });
 
 })
